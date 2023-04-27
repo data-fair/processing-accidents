500b254baf846bfdfd9d547c5e87956216e41d62
enzofcna <enzolbd@gmail.com>
2023-04-27 11:00:02 +0200

    feat : process

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = 'test'
 const config = require('config')
 const testUtils = require('@data-fair/processings-test-utils')
 const { download } = require('../lib/download.js')
+const processData = require('../lib/process.js')
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('download', () => {
   it('should download the file', async () => {
@@ -12,8 +14,24 @@ describe('download', () => {
       processingConfig: {
         year: 2020
       },
-      tmpDir: 'data/'
+      tmpDir: 'data'
     }, config, false)
     await download(context.pluginConfig, context.processingConfig, context.tmpDir, context.axios, context.log)
+  })
+})
+
+describe('process', function () {
+  it('should merge all files', async function () {
+    this.timeout(100000)
+    const context = testUtils.context({
+      pluginConfig: {
+      },
+      processingConfig: {
+        year: 2020
+      },
+      tmpDir: 'data'
+    }, config, false)
+    wait(1000000)
+    await processData(context.processingConfig, context.tmpDir, context.log)
   })
 })
