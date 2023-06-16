a4645ee0e29b392d273ddb3dd5768e341c7b3d77
enzofcna <enzolbd@gmail.com>
2023-06-16 10:26:35 +0200

    fix : content length error

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const path = require('path')
 const schema = require('./lib/schema')
 const FormData = require('form-data')
 
+function displayBytes (aSize) {
+  aSize = Math.abs(parseInt(aSize, 10))
+  if (aSize === 0) return '0 octets'
+  const def = [[1, 'octets'], [1000, 'ko'], [1000 * 1000, 'Mo'], [1000 * 1000 * 1000, 'Go'], [1000 * 1000 * 1000 * 1000, 'To'], [1000 * 1000 * 1000 * 1000 * 1000, 'Po']]
+  for (let i = 0; i < def.length; i++) {
+    if (aSize < def[i][0]) return (aSize / def[i - 1][0]).toLocaleString() + ' ' + def[i - 1][1]
+  }
+}
+
 exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patchConfig }) => {
   const formData = new FormData()
   await download(processingConfig, tmpDir, axios, log)
@@ -13,19 +22,19 @@ exports.run = async ({ pluginConfig, processingConfig, tmpDir, axios, log, patch
 
   if (processingConfig.datasetMode === 'update') {
     await log.step('Mise à jour du jeu de données')
-    await log.info('Mise à jour du schéma')
-    formData.append('schema', JSON.stringify(schema))
   } else {
     formData.append('schema', JSON.stringify(schema))
     formData.append('title', processingConfig.dataset.title)
     await log.step('Création du jeu de données')
   }
-
   const filePath = path.join(tmpDir, 'accidentsVelo.csv')
+
   formData.append('file', await fs.createReadStream(filePath), { filename: path.parse(filePath).base })
 
   formData.getLength = util.promisify(formData.getLength)
   const contentLength = await formData.getLength()
+  await log.info(`Chargement de (${displayBytes(contentLength)})`)
+
   const dataset = (await axios({
     method: 'post',
     url: (processingConfig.dataset && processingConfig.dataset.id) ? `api/v1/datasets/${processingConfig.dataset.id}` : 'api/v1/datasets',
